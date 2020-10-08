322a4b449d4f942715fcd8ec58ec8e2de620a23b
Porotikov <72504468+Porotikov@users.noreply.github.com>
2020-10-09 00:28:53 +0300

    Add files via upload

---
+++ LAB5-masterПоротиков/LAB5-master/inc/about.inc.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewpoint" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ 		<title>Abibas TABLE</title>
+		<link rel="stylesheet" type="text/css" href="../css/style.css">
+		<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
+	</head>
+
+	<body>
+
+
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<table align = "center" width = "60%" border = "1" cellpadding="35">
+	<tr>
+		<th>№</th>
+		<th>Одежда</th>
+		<th>Материал</th>
+		<th>Цена</th>
+	</tr>
+	<tr>
+		<td><strong>1</strong></td>
+		<td><strong>Футболка</strong></td>
+		<td><strong>Политилен</strong></td>
+		<td><strong>30р фирменный пакет)</strong></td>
+	</tr>
+	<tr>
+		<td><strong>2</strong></td>
+		<td><strong>Фирма Футболка</strong></td>
+		<td><strong>Хлопок 100%</strong></td>
+		<td><strong>2100р</strong></td>
+	</tr>
+	<tr>
+		<td><strong>3</strong></td>
+		<td><strong>носки</strong></td>
+		<td><strong>Секретная технология(многоразовые,без запаха)</strong></td>
+		<td><strong>1000р</strong></td>
+	</tr>
+	<tr>
+		<td><strong>4</strong></td>
+		<td><strong>Тапки</strong></td>
+		<td><strong>Резина советская</strong></td>
+		<td><strong>500р</strong></td>
+	</tr>
+	<tr>
+		<td><strong>5</strong></td>
+		<td><strong>Кепка</strong></td>
+		<td><strong>Газета</strong></td>
+		<td><strong>100р</strong></td>
+	</tr>
+	<tr>
+		<td><strong>6</strong></td>
+		<td><strong>Кепка Фирма</strong></td>
+		<td><strong>Кожа-рожа</strong></td>
+		<td><strong>10000р(потому что стиль)</strong></td>
+	</tr>
+	</table>
+			</div>
+		</div>	
+	</div>
+		
+			<footer class="page-footer font-small unique-color-dark pt-0">
+			<div class="primary-color">
+				<div class="container">
+					<div class="row py-4 d-flex align-items-center">
+						<div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+							<h6 class="mb-0 white-text">Get connected with us on social networks!</h6>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</footer>	
+
+	</body>
---
+++ LAB5-masterПоротиков/LAB5-master/inc/index.inc.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewpoint" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ 		<title>Mixail</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	</head>
+
+	<body>
+
+
+		<div class="container slider">
+			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  				<div class="carousel-inner">
+    			<div class="carousel-item active">
+     			 <img src="img/danya.jpg" class="d-block w-100" alt="...">
+    			</div>
+   				 <div class="carousel-item">
+   				 	<img src="img/mem.jpg" class="d-block w-100" alt="...">
+    			</div>
+  				</div>
+  				<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    			<span class="sr-only">Previous</span>
+ 				 </a>
+  				<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+    			<span class="sr-only">Next</span>
+  				</a>
+			</div>
+		</div>
+		<div class="container">
+						<p>Future stile<br>
+Cоздание оригинальной одежды.<br>
+Персонализация вещей. Апсайклинг. Кастомизация. Ручная роспись и вышивка. <br>Изменение дизайна. Костюм с историей. Исторический костюм.<br>
+
+По вопросам сотрудничества
+WhatsApp +79787777777
+			</p>
+		</div>
+
+		<footer class="page-footer font-small unique-color-dark pt-0">
+			<div class="primary-color">
+				<div class="container">
+					<div class="row py-4 d-flex align-items-center">
+						<div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+							<h6 class="mb-0 white-text">Get connected with us on social networks!</h6>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</footer>
+	</body>	
+
+</html>	
+ 
